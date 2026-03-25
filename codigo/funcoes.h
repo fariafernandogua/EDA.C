61db9dbeefcc568a66ff9d66aa6352bfd96d7330
fariafernandogua <guafariafernando26@gmail.com>
2026-03-25 23:34:17 +0200

    Add function prototypes for factorial and Fibonacci

--- codigo/funcoes.h
+++ codigo/funcoes.h
@@ -1,1 +1,11 @@
+#ifndef FUNCOES_H
+#define FUNCOES_H
 
+
+void fatorial_iterativo(int n);
+void fatorial_recursivo(int n);
+
+void fibonacci_iterativo(int n);
+void fibonacci_recursivo(int n);
+
+#endif
